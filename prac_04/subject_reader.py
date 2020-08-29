d79fd88fb2a015328af13bb8cb88a2664584f4a5
yanimaze <68747499+yanimaze@users.noreply.github.com>
2020-08-29 23:25:01 +1000

    get_data function to return the data as list of list

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -14,16 +14,19 @@ def main():
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    parts_list = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        # print(parts)  # See if that worked
+        # print("----------")
+        parts_list.append(parts)
     input_file.close()
+    return parts_list
 
 
 main()
