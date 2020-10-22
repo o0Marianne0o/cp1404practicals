771fbeb44f69940466d26d3bc8668391a4210954
yanimaze <68747499+yanimaze@users.noreply.github.com>
2020-10-23 00:27:16 +1000

    programming_language.py and languages.py class defined

---
+++ prac_06/programming_language.py
@@ -0,0 +1,23 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return "{}, {} Typing, Reflection={}, First appeared in {}".format(
+            self.name, self.typing, self.reflection, self.year)
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+print(python)
+
+programming_languages = [ruby, python, visual_basic]
